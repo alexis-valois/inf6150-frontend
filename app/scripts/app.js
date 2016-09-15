69287842326594591ed824038d3f345f653fc475
Alexis Valois-Adamowicz <valois-adamowicz.alexis@courrier.uqam.ca>
2016-09-15 18:24:06 -0400

    Base scaffolding for angular webapp using yeoman tool

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name inf6150FrontendApp
+ * @description
+ * # inf6150FrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('inf6150FrontendApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name inf6150FrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the inf6150FrontendApp
+ */
+angular.module('inf6150FrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
