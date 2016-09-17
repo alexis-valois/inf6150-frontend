498058b1e97732847b709f36dad48f6fe4d8cb85
Alexis Valois-Adamowicz <valois-adamowicz.alexis@courrier.uqam.ca>
2016-09-17 18:40:57 -0400

    WIP : login + registration process

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,9 +17,15 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'LocalStorageModule',
+    'UserServices'
+
   ])
-  .config(function ($routeProvider) {
+  .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('EB');
+  }])
+  .config(function ($routeProvider, $httpProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -31,7 +37,50 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'UserCtrl',
+        controllerAs: 'user'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'UserCtrl',
+        controllerAs: 'register'
+      })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/login'
       });
+      
+      $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+    
+  })
+  .run(function($rootScope, $http, $location, localStorageService) {
+     $rootScope.logout = function() {
+      console.log('logout test');
+      $http.post('http://localhost:8081/user/logout', {})
+        .success(function() {         
+            $location.path('/');
+            $rootScope.loggedOut = true;
+        })
+        .error(function() {
+            $rootScope.error = true;
+            console.log('logout error');
+        });
+        localStorageService.set('user', null);
+        localStorageService.set('authenticated', false);
+        $http.defaults.headers.common.sessionToken = null;
+        $rootScope.authenticated = false;
+    };
+
+    $rootScope.$on('$routeChangeSuccess', function () {
+        $rootScope.authenticated = localStorageService.get('authenticated');
+        $rootScope.user = localStorageService.get('user');
+        $rootScope.error = false;
+        if ($rootScope.user !== null){
+          $http.defaults.headers.common.sessionToken = localStorageService.get('user').sessionToken;
+        }
+        if ($rootScope.loggedOut){
+          $rootScope.loggedOut = false;
+        }        
+    });
   });
