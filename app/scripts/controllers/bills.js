ad9f86cba83b8d4001f515e0c689e7f6b33407a0
Mathieu08 <mat.caouette08@gmail.com>
2016-11-19 12:20:13 -0500

    Adaptation du frontend avec le backend.

--- app/scripts/controllers/bills.js
+++ app/scripts/controllers/bills.js
@@ -11,23 +11,66 @@ angular.module('inf6150FrontendApp')
   .controller('BillsCtrl', ['$scope', 'BillsServices', 'CategoriesService', 'SuppliersServices', 'AccountService',
   	function ($scope, BillsServices, CategoriesService, SuppliersServices, AccountService) {
     	
-    	$scope.bills = BillsServices.findAll();
+    	BillsServices.findAll().$promise.then(function(data){
+        $scope.bills = data;
+      });    
 
-    	$scope.categories = CategoriesService.findAll();
+    	CategoriesService.findAll().$promise.then(function(data){
+        $scope.selectedCategorie = data[0].id;
+        $scope.categories = data;
+      });    
 
-    	$scope.suppliers = SuppliersServices.findAll();
+    	SuppliersServices.findAll().$promise.then(function(data){
+        $scope.selectedSupplier = data[0].id;
+        $scope.suppliers = data;
+      });    
 
-    	$scope.accounts = AccountService.findAll();
+    	AccountService.findAll().$promise.then(function(data){
+        $scope.selectedAccount = data[0].id;
+        $scope.accounts = data;
+        $scope.selectedCurrency = data[0].initAmount.currency;
+      });    
+
+      $scope.selectAccount = function(accountId){
+        $scope.selectedAccount = accountId;
+      };
+
+      $scope.selectCategorie = function(categorieId){
+        $scope.selectedCategorie = categorieId;
+      };
+
+      $scope.selectSupplier = function(supplierId){
+        $scope.selectedSupplier = supplierId;
+      };
+
+      $scope.selectCurrency = function(currency){
+          $scope.selectedCurrency = currency;
+      };
 
     	$scope.addEmptyBill = function(){
   			$scope.bills.push({});
   		};
 
   		$scope.createOrUpdate = function(bill){
+        bill.accountId = $scope.selectedAccount;
+        bill.categorieId = $scope.selectedCategorie;
+        bill.supplierId = $scope.selectedSupplier;
+        bill.amount.currency = $scope.selectedCurrency;
+        
   			if (bill.id){
-  				BillsServices.update({ id: bill.id }, bill);
+  				BillsServices.update({ id: bill.id }, bill)
+              .$promise.then(
+                        function(){
+                            $scope.bills = BillsServices.findAll();
+                        }
+                    );
   			}else{
-  				BillsServices.create(bill);
+  				BillsServices.create(bill)
+              .$promise.then(
+                        function(){
+                            $scope.bills = BillsServices.findAll();
+                        }
+                    );
   			}  			
   		};
 
